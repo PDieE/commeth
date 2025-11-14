88cb6637b88b552b35f49da25dddea9539fa336c
黄潇逸 <23275429@qq.com>
2025-11-14 09:34:43 +0800

    fix: 打包类型文件恢复

--- packages/components-antv/vite.config.ts
+++ packages/components-antv/vite.config.ts
@@ -1,46 +1,37 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import dts from 'unplugin-dts/vite'
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import dts from "unplugin-dts/vite";
+import { resolve } from "path";
 
 export default defineConfig({
   build: {
     cssCodeSplit: true,
     lib: {
-      entry: resolve(__dirname, 'main.ts'),
-      name: 'CommethComponentsAntv',
-      formats: ['es'],
-      fileName: 'index',
+      entry: resolve(__dirname, "main.ts"),
+      name: "CommethComponentsAntv",
+      formats: ["es"],
+      fileName: "index",
     },
     rollupOptions: {
-      external: ['vue'],
+      external: ["vue"],
       output: [
         {
-            format: "es",
-            entryFileNames: "[name].js",
-            exports: "named",
-            name: "CommethComponentsAntv",
-            dir: "./dist",
+          format: "es",
+          entryFileNames: "[name].js",
+          exports: "named",
+          preserveModules: true,
+          preserveModulesRoot: ".",
+          dir: "./dist/es",
         },
-        {
-            format: "es",
-            entryFileNames: "[name].js",
-            exports: "named",
-            preserveModules: true,
-            preserveModulesRoot: ".",
-            dir: "./dist/es",
-        },
-        // {
-        //     format: "cjs",
-        //     entryFileNames: "[name].js",
-        //     exports: "named",
-        //     preserveModules: true,
-        //     preserveModulesRoot: "packages",
-        //     dir: "./dist/lib",
-        // },
-     ],
-    }
+      ],
+    },
   },
-  plugins: [vue(), dts({ tsconfigPath: './tsconfig.prod.json', processor: 'vue' })]
+  plugins: [
+    vue(),
+    dts({
+      tsconfigPath: "./tsconfig.prod.json",
+      processor: "vue",
+      outDirs: ["./dist/es"],
+    }),
+  ],
 });
- 
